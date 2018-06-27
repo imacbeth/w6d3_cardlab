193aa4ac08e8605ba32fc39ad879bc79436fee3e
Can Toraman <cantoraman@gmail.com>
2018-06-27 10:30:37 +0100

    Card Test returns fine

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,2 +1,17 @@
 public class Card {
+
+    private SuitType suit;
+    private RankType rank;
+
+
+    public Card(SuitType suit, RankType rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public SuitType getSuit() {
+        return suit;
+    }
+
+
 }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CardTest {
+
+    Card card;
+
+    @Before
+    public void before(){
+        card = new Card(SuitType.HEARTS, RankType.QUEEN);
+    }
+
+    @Test
+    public void canGetSuit(){
+        assertEquals(SuitType.HEARTS, card.getSuit());
+    }
+
+
+
+
 }
